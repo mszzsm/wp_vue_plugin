71ee5be5b0d07ab5a99616add4877165dc925e6b
msz <mikeshtefanitsa@gmail.com>
2019-08-19 17:13:51 +0200

    Update vue-plugin.php

--- vue-plugin/vue-plugin.php
+++ vue-plugin/vue-plugin.php
@@ -7,12 +7,15 @@ Version: 1.0
 
 //shortcode creation 
 function handle_shortcode() {
-    return '<div id="mount">Mount DIV</div>';
-	
+    return '<div id="mount">Mount DIV</div>' ;
 }
 add_shortcode('latestPosts', 'handle_shortcode');
 
 
+
+function my_stylesheet(){
+    wp_register_style('styles', plugin_dir_url( __FILE__ ) .'/assets/style.css');
+    wp_enqueue_style('styles');
 //create vue connection 
 function enqueue_scripts(){
    global $post;
@@ -23,3 +26,25 @@ function enqueue_scripts(){
 };
 
 add_action('wp_enqueue_scripts', 'enqueue_scripts'); 
+add_action('wp_enqueue_scripts', 'my_stylesheet' );
+
+add_action('rest_api_init', 'register_rest_images' );
+
+function register_rest_images(){
+    register_rest_field( array('post'),
+        'fimg_url',
+        array(
+            'get_callback'    => 'get_rest_featured_image',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
+
+function get_rest_featured_image( $object, $field_name, $request ) {
+    if( $object['featured_media'] ){
+        $img = wp_get_attachment_image_src( $object['featured_media'], 'app-thumb' );
+        return $img[0];
+    }
+    return false;
+}
