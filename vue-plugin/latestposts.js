91fea1b6aa9631e8560dafdcc2c917c6df721222
msz <mikeshtefanitsa@gmail.com>
2019-08-20 10:56:27 +0200

    Update latestposts.js

--- vue-plugin/latestposts.js
+++ vue-plugin/latestposts.js
@@ -27,36 +27,39 @@
             },
 			image: 'https://picsum.photos/id/238/200/100',
 			views: 0,
+			first: 'item1',
+			OffSet: 0,
+			perPage: 13,
+			
         },
 
         template: `<div class="wrapper">
 			<!-- Sidebar -->
-		<nav id="sidebar">
-            <h1>{{activeCategory}}</h1>
-        
-            <button class="btn btn-active" @click="changeCategory('')">All </button>
-            <button class="btn" v-for="category in categoryNames" :value="category"
-                @click="changeCategory(category)">
-                <span> {{ category }} </span>
-            </button>
-         </nav>
-			<div class="content">
-				  <div class="active_cat">{{activeCategory}}</div>
-                <article>
-					<div class="grid-container">
-						<div v-for="post in filteredPosts" class="post grid-item" >
-                        	<span class="badge" v-for="category in post.categories"
-                            	v-bind:class="color[getCatName(post.categories)]">
-                            	{{getCatName(post.categories)}}
-                        	</span>
-                            <div class="title">{{post.title.rendered}}</div>
-                            <div class="author">{{getUserId(post.author)}}</div>
-                            <a v-bind:href="post.link"><img class="grayimg" src="https://picsum.photos/id/18/600/300" alt="img"></a>
-						</div>
+				<div id="sidebar-min">{{activeCategory}}</div>
+					<nav id="sidebar">
+						<button class="btn btn-active" @click="changeCategory('')">All </button>
+						<button class="btn" v-for="category in categoryNames" :value="category"
+							@click="changeCategory(category)">
+							<span> {{ category }} </span>
+						</button>
+					</nav>
+					<div class="content">
+						<div class="active_cat">{{activeCategory}}</div>
+						<article>
+							<div class="grid-container">
+								<div v-for="(post, index) in filteredPosts"  class="post grid-item" :class="{'item1': index === 0}">
+										<span class="badge" v-for="category in post.categories"
+											v-bind:class="color[getCatName(post.categories)]">
+											{{getCatName(post.categories)}}
+										</span>
+										<div class="title">{{post.title.rendered}}</div>
+										<div class="author">{{getUserId(post.author)}}</div>
+										<a v-bind:href="post.link"><img class="grayimg" :src="post.fimg_url" alt="img"></a>
+								</div>
+							</div>
+						</article>
 					</div>
-				</article>
-            </div>
-        </div>`,
+				</div>`,
 
  
 		
@@ -73,7 +76,7 @@
 	
         methods: {
             fetchPosts: function () {
-             var url = '/wp-json/wp/v2/posts?per_page=100';
+				var url = '/wp-json/wp/v2/posts?per_page=' + this.perPage + '&offset=' + this.OffSet;
              fetch(url).then((response) => { return response.json() }).then((data) => {this.posts = data;});
             }, 
 
@@ -124,7 +127,7 @@
                     return this.posts;
                 } else {
                     var postId = this.getCatId(this.activeCategory)
-					var id = this.posts.filter((Post) => Post.categories == postId);
+					var id = this.posts.filter((Post) => Post.categories[0] == postId);
                     return id;
                 }
 			},
